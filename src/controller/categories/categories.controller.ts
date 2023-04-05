dd0dc004a004a2c6e3fd30c34a557dc530a91500
Lion <breakermoob@hotmail.com>
2023-04-05 01:00:34 -0500

    feat: products crud complete

--- src/controller/categories/categories.controller.ts
+++ src/controller/categories/categories.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+  @Get(':id/products/:productId') getCategories(
+    @Param('id') id: string,
+    @Param('productId') productId: string,
+  ) {
+    return `Category: ${id}, Product: ${productId}`;
+  }
+}
--- src/controller/products/products.controller.ts
+++ src/controller/products/products.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  @Get() getProducts(
+    @Query() limite: number,
+    @Query() offset: number,
+    @Query() brand: string,
+  ) {
+    return `Products: limite: ${limite}, offset: ${offset}, brand: ${brand}`;
+  }
+
+  @Get('filter') getProductsFilter() {
+    return `Products filter`;
+  }
+
+  @Get(':id') getProduct(@Param('id') id: string) {
+    return `Product: ${id}`;
+  }
+
+  @Post() create(@Body() payload: any) {
+    return {
+      ...payload,
+      message: 'action create',
+    };
+  }
+
+  @Put(':id') update(@Param('id') id: string, @Body() payload: any) {
+    return {
+      id,
+      ...payload,
+      message: 'action create',
+    };
+  }
+
+  @Delete(':id') delete(@Param('id') id: string) {
+    return `Product: ${id} has been deleted`;
+  }
+}
