018ce48cd54195b9f35efe7f8b0eb077269983ba
Lion <breakermoob@hotmail.com>
2023-04-06 03:39:16 -0500

    feat: property for alert about non necessary fields

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // remove all properties that are not defined in the DTO
+      forbidNonWhitelisted: true, // throw an error if a property is not defined in the DTO
     }),
   );
   await app.listen(3000);
