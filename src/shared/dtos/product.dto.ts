84a44d68d2fb893b1bb830e722208278156960f3
Lion <breakermoob@hotmail.com>
2023-04-06 03:31:21 -0500

    feat: Validations for DTOs

--- src/shared/dtos/product.dto.ts
+++ src/shared/dtos/product.dto.ts
@@ -1,15 +1,32 @@
+import { IsNotEmpty, IsNumber, IsString, IsUrl } from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
+  @IsNumber()
+  @IsNotEmpty()
   readonly price: number;
+  @IsNumber()
+  @IsNotEmpty()
   readonly stock: number;
+  @IsUrl()
+  @IsNotEmpty()
   readonly image: string;
 }
 
 export class UpdateProductDto {
+  @IsString()
   readonly name?: string;
+  @IsString()
   readonly description?: string;
+  @IsNumber()
   readonly price?: number;
+  @IsNumber()
   readonly stock?: number;
+  @IsUrl()
   readonly image?: string;
 }
