37ad69f9e36f8349a28f21476eda92941d676551
Lion <breakermoob@hotmail.com>
2023-04-08 14:40:19 -0500

    feat: sagger api properties

--- src/modules/products/dtos/product.dto.ts
+++ src/modules/products/dtos/product.dto.ts
@@ -1,21 +1,26 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString, IsUrl } from 'class-validator';
 
 export class CreateProductDto {
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ description: 'Name of the product' })
   readonly name: string;
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ description: 'Description of the product' })
   readonly description: string;
   @IsNumber()
   @IsNotEmpty()
+  @ApiProperty({ description: 'Price of the product' })
   readonly price: number;
   @IsNumber()
   @IsNotEmpty()
+  @ApiProperty({ description: 'Stock of the product' })
   readonly stock: number;
   @IsUrl()
   @IsNotEmpty()
+  @ApiProperty({ description: 'Image of the product' })
   readonly image: string;
 }
 
