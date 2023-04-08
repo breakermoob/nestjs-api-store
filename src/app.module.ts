bf949e1844672dd8e43dca749f816e8e2170065b
Lion <breakermoob@hotmail.com>
2023-04-08 14:00:23 -0500

    feat: environments values with dotenv across nestjs config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CategoriesController } from './controller/categories/categories.controller';
-import { ProductsController } from './controller/products/products.controller';
+import { environment } from './environments';
+import { DatabaseModule } from './modules/database/database.module';
 import { ProductsModule } from './modules/products/products.module';
 import { ProductService } from './modules/products/services/product.service';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [ProductsModule, UsersModule],
-  controllers: [AppController, ProductsController, CategoriesController],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: environment[process.env.NODE_ENV] || '.env',
+      isGlobal: true,
+    }),
+    ProductsModule,
+    UsersModule,
+    DatabaseModule,
+  ],
+  controllers: [AppController],
   providers: [AppService, ProductService],
 })
 export class AppModule {}
