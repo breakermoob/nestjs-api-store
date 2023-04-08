4ed82dc4df08bb587bf30ce1ea89bc9147ebc761
Lion <breakermoob@hotmail.com>
2023-04-08 14:34:25 -0500

    feat: Swagger docs config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configuración Swagger en NestJS
+  const config = new DocumentBuilder()
+    .setTitle('Store API')
+    .setDescription('Documentación Store')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  // URL API
+  SwaggerModule.setup('docs', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // remove all properties that are not defined in the DTO
