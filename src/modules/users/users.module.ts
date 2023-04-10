ae73a21a5fda9f4906e6d99ddf84ae004261aa1c
Lion <breakermoob@hotmail.com>
2023-04-10 02:53:12 -0500

    feat: TypeORM OneToOne Relationships

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from '../products/products.module';
 import { CustomerController } from './controllers/customers.controller';
 import { UsersController } from './controllers/users.controller';
+import { Customer } from './entities/customer.entity';
+import { User } from './entities/user.entity';
 import { CustomersService } from './services/customers.service';
 import { UsersService } from './services/users.service';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, TypeOrmModule.forFeature([User, Customer])],
   controllers: [CustomerController, UsersController],
   providers: [CustomersService, UsersService],
 })
